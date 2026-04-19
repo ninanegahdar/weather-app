6cf1473bbbb07c9eb1fbac9557d4e09246676620
nina negahdar <bhm577@myy.haaga-helia.fi>
2026-04-19 19:12:23 +0300

    deleted Home.tsx and made index.tsx the homepage. Added and implemented weather API to location function.

---
+++ Documents/9/School/Kevät26/Mobiiliohjelmointi/Lopputyö/weather-app/app/(tabs)/index.tsx
@@ -0,0 +1,81 @@
+import { useEffect, useState } from "react";
+import { Text, View, StyleSheet } from "react-native";
+
+import * as Location from "expo-location";
+
+export default function Home() {
+  const API_KEY = "797d87539045726c3e9d6c13055395f3";
+
+  const [location, setLocation] = useState<Location.LocationObject | null>(null);
+  const [errorMsg, setErrorMsg] = useState<string | null>(null);
+  const [weather, setWeather] = useState<any>(null);
+
+useEffect(() => {
+    async function getCurrentLocation() {
+      
+      let { status } = await Location.requestForegroundPermissionsAsync();
+      if (status !== 'granted') {
+        setErrorMsg('Permission to access location was denied');
+        return;
+      }
+
+      let location = await Location.getCurrentPositionAsync({});
+      setLocation(location);
+    }
+
+    getCurrentLocation();
+  }, []);
+
+  useEffect(() => {
+  if (!location) return;
+
+  async function fetchWeather() {
+    const lat = location!.coords.latitude;
+    const lon = location!.coords.longitude;
+
+    const response = await fetch(
+      `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&units=metric&appid=${API_KEY}`
+    );
+
+    const data = await response.json();
+    setWeather(data);
+  }
+
+  fetchWeather();
+}, [location]);
+
+return (
+  <View style={styles.container}>
+    {weather ? (
+      <Text style={styles.text}>
+        🌍 {weather.name}{"\n"}
+        🌡️ {weather.main.temp} °C{"\n"}
+        ☁️ {weather.weather[0].description}
+      </Text>
+    ) : location ? (
+      <Text style={styles.text}>Loading weather...</Text>
+    ) : errorMsg ? (
+      <Text style={styles.text}>{errorMsg}</Text>
+    ) : (
+      <Text style={styles.text}>Loading location...</Text>
+    )}
+  </View>
+);
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+    padding: 20,
+  },
+  text: {
+    fontSize: 18,
+    textAlign: 'center',
+    color: 'white',
+  },
+});
+
+
+//https://docs.expo.dev/versions/latest/sdk/location/
---
+++ Documents/9/School/Kevät26/Mobiiliohjelmointi/Lopputyö/weather-app/diary.md
@@ -0,0 +1,7 @@
+Oppimispäiväkirja
+
+Day 1:
+Loin uuden github repon ja loin projektin. Suunnittelin alkuun seuraavat näkymät: 1. Home - sen hetkisen lokaation sää, 2. Haku ja 3. Suosikit. Otin selvää Expo Locationista ja yritin ymmärtää miten se toimii.
+
+Day2:
+Hain OpenWeather:sta oman API Keyn säätä varten ja yritin saada sen toimimaan oikein kotinäkymässä, kun käyttäjä antaa luvan sijaintiin.
